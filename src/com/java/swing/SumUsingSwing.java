d55a842b1409b0ff3a8c34d12a4be337aca36224
paribartan <paribartankalathoki@gmail.com>
2019-09-11 22:06:20 +0545

    removed unnecessary import

--- src/com/java/swing/SumUsingSwing.java
+++ src/com/java/swing/SumUsingSwing.java
@@ -3,12 +3,10 @@ package com.java.swing;
 import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 /**
